cc5e09b323157171a5b67522aef60856e8ad57cc
Minh Dang <dang.repleh@gmail.com>
2022-11-11 08:33:06 +0700

    Data gen update

--- crowd_nav/train.py
+++ crowd_nav/train.py
@@ -10,15 +10,13 @@ import torch
 import gym
 import sys
 
-from crowd_nav.utils.misc import *
-
 sys.path.append('../')
 from crowd_sim.envs.utils.robot import Robot
 from crowd_nav.utils.trainer import Trainer
 from crowd_nav.utils.memory import ReplayMemory
 from crowd_nav.utils.explorer import Explorer
 from crowd_nav.policy.policy_factory import policy_factory
-
+from crowd_nav.utils.misc import *
 
 def main():
     parser = argparse.ArgumentParser('Parse configuration file')
